9cd9f3f58563992d3fea2f193db3ee1a401ce3bc
Fratsie <mats.vanspringel2006@gmail.com>
2025-02-12 15:03:47 +0100

    dropdown fully fixed

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function dropdown(bool){
-    if(bool){
+var dropdownBool = true;
+
+function dropdown(){
+    if(dropdownBool){
         document.getElementById('navList').style.display = 'block';
+        document.getElementById('dropdown').style.rotate = "180deg";
+        dropdownBool = false;
     }
     else{
         document.getElementById('navList').style.display = 'none';
+        document.getElementById('dropdown').style.rotate = "0deg";
+        dropdownBool = true;
     }
 }
